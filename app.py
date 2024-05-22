a99fd7dbfea8cb91985ebb62bbae39a3b99d7e34
klayhopgood <klaychop1@gmail.com>
2024-05-22 17:08:52 +0200

    hgkj

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-import json
-import os
 import stripe
-import secrets
+from flask import Flask, request, jsonify, render_template, session, redirect, url_for
 import requests
-import datetime
 import mysql.connector
-from flask import Flask, request, redirect, jsonify, render_template, session, url_for
+from mysql.connector import errorcode
+import datetime
 from dateutil import parser
+import secrets
 from flask_session import Session
 
 app = Flask(__name__)
@@ -14,9 +13,9 @@ app.secret_key = secrets.token_hex(16)
 app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 
-# Stripe configuration
+# Stripe credentials
 stripe.api_key = 'sk_test_51PJDimAlw5arL9EanWVm9Jg9yF5ZiFgnLx3tzh5Snx2fbW2TduAATIB1Lzmf4gQiYscwGRKZxavu89UVqubbjbqh00dRAB8Kme'
-stripe_endpoint_secret = 'whsec_SpYHjmZTR6G7iowQFSHSkXngW4ewqRLr'
+endpoint_secret = 'whsec_SpYHjmZTR6G7iowQFSHSkXngW4ewqRLr'
 
 # Database configuration
 db_config = {
@@ -56,34 +55,23 @@ def login_callback():
         response = requests.post(token_url, data=payload)
         if response.status_code == 200:
             tokens = response.json()
-            session['access_token'] = tokens.get('access_token')
-            session['refresh_token'] = tokens.get('refresh_token')
-            session['expires_at'] = tokens.get('expires_at')
-            session['athlete_id'] = str(tokens.get('athlete').get('id'))
-            save_tokens_to_db(session['athlete_id'], session['access_token'], session['refresh_token'], session['expires_at'])
-            preferences = get_user_preferences(session['athlete_id'])
+            access_token = tokens.get('access_token')
+            refresh_token = tokens.get('refresh_token')
+            expires_at = tokens.get('expires_at')
+            athlete_id = str(tokens.get('athlete').get('id'))
+
+            session['access_token'] = access_token
+            session['refresh_token'] = refresh_token
+            session['expires_at'] = expires_at
+            session['athlete_id'] = athlete_id
+
+            save_tokens_to_db(athlete_id, access_token, refresh_token, expires_at)
+            preferences = get_user_preferences(athlete_id)
             return render_template('index.html', preferences=preferences)
         else:
             return 'Failed to login. Error: ' + response.text
     else:
         return 'Authorization code not received.'
-
-@app.route('/deauthorize')
-def deauthorize():
-    access_token = session.get('access_token')
-    if access_token:
-        deauthorize_url = 'https://www.strava.com/oauth/deauthorize'
-        response = requests.post(deauthorize_url, data={'access_token': access_token})
-        if response.status_code == 200:
-            session.pop('access_token', None)
-            session.pop('refresh_token', None)
-            session.pop('expires_at', None)
-            session.pop('athlete_id', None)
-            return redirect('/')
-        else:
-            return 'Failed to deauthorize. Error: ' + response.text
-    else:
-        return redirect('/')
 
 def save_tokens_to_db(athlete_id, access_token, refresh_token, expires_at):
     try:
@@ -122,104 +110,10 @@ def get_tokens_from_db(owner_id):
         if connection:
             connection.close()
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    payload = request.get_data(as_text=True)
-    sig_header = request.headers.get('Stripe-Signature')
-
-    try:
-        event = stripe.Webhook.construct_event(
-            payload, sig_header, stripe_endpoint_secret
-        )
-    except ValueError as e:
-        print(f"Error: {e}")
-        return 'Invalid payload', 400
-    except stripe.error.SignatureVerificationError as e:
-        print(f"Error while verifying webhook signature: {e}")
-        return 'Invalid signature', 400
-
-    if event['type'] == 'checkout.session.completed':
-        session = event['data']['object']
-        client_reference_id = session.get('client_reference_id')
-        if client_reference_id:
-            update_paid_status(client_reference_id, True)
-
-    return 'Success', 200
-
-def update_paid_status(athlete_id, is_paid):
-    try:
-        connection = get_db_connection()
-        cursor = connection.cursor()
-        cursor.execute("""
-            UPDATE strava_tokens
-            SET is_paid_user = %s
-            WHERE athlete_id = %s
-        """, (is_paid, athlete_id))
-        connection.commit()
-    except mysql.connector.Error as err:
-        print(f"Error: {err.msg}")
-    finally:
-        if cursor:
-            cursor.close()
-        if connection:
-            connection.close()
-
-@app.route('/create-checkout-session', methods=['POST'])
-def create_checkout_session():
-    athlete_id = session.get('athlete_id')
-    if not athlete_id:
-        return 'User not authenticated', 403
-
-    try:
-        checkout_session = stripe.checkout.Session.create(
-            payment_method_types=['card'],
-            mode='subscription',
-            line_items=[{
-                'price': 'price_1PJEMaAlw5arL9Eaq14rYBu1',
-                'quantity': 1,
-            }],
-            success_url=url_for('subscription_success', _external=True) + '?session_id={CHECKOUT_SESSION_ID}',
-            cancel_url=url_for('subscription_cancel', _external=True),
-            client_reference_id=athlete_id
-        )
-        return redirect(checkout_session.url, code=303)
-    except Exception as e:
-        print(f"Error creating Stripe Checkout session: {e}")
-        return str(e), 500
-
-@app.route('/subscription-success')
-def subscription_success():
-    return "Subscription successful! You are now a paid user."
-
-@app.route('/subscription-cancel')
-def subscription_cancel():
-    return "Subscription cancelled."
-
-@app.route('/unsubscribe', methods=['POST'])
-def unsubscribe():
-    athlete_id = session.get('athlete_id')
-    if not athlete_id:
-        return 'User not authenticated', 403
-
-    try:
-        tokens = get_tokens_from_db(athlete_id)
-        if tokens and tokens.get('stripe_customer_id'):
-            subscriptions = stripe.Subscription.list(customer=tokens['stripe_customer_id'])
-            for sub in subscriptions:
-                stripe.Subscription.delete(sub.id)
-            update_paid_status(athlete_id, False)
-            return redirect('/')
-        else:
-            return 'No active subscription found.', 400
-    except Exception as e:
-        print(f"Error during unsubscription: {e}")
-        return str(e), 500
-
 @app.route('/update_preferences', methods=['POST'])
 def update_preferences():
-    athlete_id = session.get('athlete_id')
-    print(f"Session Athlete ID: {athlete_id}")  # Debugging print statement
-    if not athlete_id:
+    owner_id = session.get('athlete_id')
+    if not owner_id:
         return 'User not authenticated', 403
 
     preferences = {
@@ -252,114 +146,81 @@ def update_preferences():
                 beers_burnt = VALUES(beers_burnt),
                 pizza_slices_burnt = VALUES(pizza_slices_burnt),
                 remove_promo = VALUES(remove_promo)
-        """, (athlete_id, preferences['days_run'], preferences['total_kms'], preferences['avg_kms'], preferences['total_elevation'], preferences['avg_elevation'], preferences['avg_pace'], preferences['avg_pace_per_week'], preferences['beers_burnt'], preferences['pizza_slices_burnt'], preferences['remove_promo']))
+        """, (owner_id, preferences['days_run'], preferences['total_kms'], preferences['avg_kms'], preferences['total_elevation'], preferences['avg_elevation'], preferences['avg_pace'], preferences['avg_pace_per_week'], preferences['beers_burnt'], preferences['pizza_slices_burnt'], preferences['remove_promo']))
         connection.commit()
-        return render_template('index.html', preferences=preferences, updated=True)
     except mysql.connector.Error as err:
         print(f"Error: {err.msg}")
-        return 'Database error', 500
     finally:
         if cursor:
             cursor.close()
         if connection:
             connection.close()
 
-def get_user_preferences(owner_id):
-    try:
-        connection = get_db_connection()
-        cursor = connection.cursor(dictionary=True)
-        cursor.execute("SELECT * FROM user_preferences WHERE owner_id = %s", (owner_id,))
-        result = cursor.fetchone()
-        cursor.close()
-        if result:
-            return {
-                'days_run': result.get('days_run', True),
-                'total_kms': result.get('total_kms', True),
-                'avg_kms': result.get('avg_kms', True),
-                'total_elevation': result.get('total_elevation', False),
-                'avg_elevation': result.get('avg_elevation', False),
-                'avg_pace': result.get('avg_pace', False),
-                'avg_pace_per_week': result.get('avg_pace_per_week', False),
-                'beers_burnt': result.get('beers_burnt', False),
-                'pizza_slices_burnt': result.get('pizza_slices_burnt', False),
-                'remove_promo': result.get('remove_promo', False),
-                'is_paid_user': check_if_paid_user(owner_id)
-            }
-        else:
-            return {
-                'days_run': True,
-                'total_kms': True,
-                'avg_kms': True,
-                'total_elevation': False,
-                'avg_elevation': False,
-                'avg_pace': False,
-                'avg_pace_per_week': False,
-                'beers_burnt': False,
-                'pizza_slices_burnt': False,
-                'remove_promo': False,
-                'is_paid_user': check_if_paid_user(owner_id)
-            }
-    except mysql.connector.Error as err:
-        print(f"Error: {err.msg}")
-        return {
-            'days_run': True,
-            'total_kms': True,
-            'avg_kms': True,
-            'total_elevation': False,
-            'avg_elevation': False,
-            'avg_pace': False,
-            'avg_pace_per_week': False,
-            'beers_burnt': False,
-            'pizza_slices_burnt': False,
-            'remove_promo': False,
-            'is_paid_user': False
-        }
-    finally:
-        if connection:
-            connection.close()
+    return render_template('index.html', preferences=preferences, updated=True)
 
-def check_if_paid_user(owner_id):
-    try:
-        connection = get_db_connection()
-        cursor = connection.cursor(dictionary=True)
-        cursor.execute("SELECT is_paid_user FROM strava_tokens WHERE owner_id = %s", (owner_id,))
-        result = cursor.fetchone()
-        cursor.close()
-        return result['is_paid_user'] == 1 if result else False
-    except mysql.connector.Error as err:
-        print(f"Error: {err.msg}")
-        return False
-    finally:
-        if connection:
-            connection.close()
-
-@app.route('/subscription-success')
-def subscription_success():
-    return "Subscription successful! You are now a paid user."
-
-@app.route('/subscription-cancel')
-def subscription_cancel():
-    return "Subscription cancelled."
-
-@app.route('/unsubscribe', methods=['POST'])
-def unsubscribe():
+@app.route('/create-checkout-session', methods=['POST'])
+def create_checkout_session():
     athlete_id = session.get('athlete_id')
     if not athlete_id:
-        return 'User not authenticated', 403
+        return jsonify({'error': 'User not authenticated'}), 403
 
     try:
-        tokens = get_tokens_from_db(athlete_id)
-        if tokens and tokens.get('stripe_customer_id'):
-            subscriptions = stripe.Subscription.list(customer=tokens['stripe_customer_id'])
-            for sub in subscriptions:
-                stripe.Subscription.delete(sub.id)
-            update_paid_status(athlete_id, False)
-            return redirect('/')
-        else:
-            return 'No active subscription found.', 400
+        checkout_url = f"https://buy.stripe.com/test_aEUeXf8AVcvVdA4000?client_reference_id={athlete_id}"
+        return jsonify({'url': checkout_url})
     except Exception as e:
-        print(f"Error during unsubscription: {e}")
-        return str(e), 500
+        return jsonify(error=str(e)), 403
+
+@app.route('/webhook', methods=['POST'])
+def stripe_webhook():
+    payload = request.get_data(as_text=True)
+    sig_header = request.headers.get('Stripe-Signature')
+
+    try:
+        event = stripe.Webhook.construct_event(payload, sig_header, endpoint_secret)
+    except ValueError as e:
+        print(f"Error while parsing webhook: {e}")
+        return 'Invalid payload', 400
+    except stripe.error.SignatureVerificationError as e:
+        print(f"Error while verifying webhook signature: {e}")
+        return 'Invalid signature', 400
+
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+        customer_id = session['customer']
+        client_reference_id = session.get('client_reference_id')
+
+        if client_reference_id:
+            try:
+                connection = get_db_connection()
+                cursor = connection.cursor()
+                cursor.execute("""
+                    UPDATE strava_tokens SET is_paid_user = 1, stripe_customer_id = %s WHERE athlete_id = %s
+                """, (customer_id, client_reference_id))
+                connection.commit()
+            except mysql.connector.Error as err:
+                print(f"Error: {err.msg}")
+            finally:
+                if cursor:
+                    cursor.close()
+                if connection:
+                    connection.close()
+
+    return '', 200
+
+@app.route('/webhook', methods=['GET', 'POST'])
+def webhook():
+    if request.method == 'GET':
+        verify_token = request.args.get('hub.verify_token')
+        challenge = request.args.get('hub.challenge')
+        if verify_token == VERIFY_TOKEN:
+            return jsonify({'hub.challenge': challenge})
+        return 'Invalid verification token', 403
+    elif request.method == 'POST':
+        event = request.json
+        print(f"Received event: {event}")
+        if event['object_type'] == 'activity' and event['aspect_type'] == 'create':
+            handle_activity_create(event['object_id'], event['owner_id'])
+        return 'Event received', 200
 
 def handle_activity_create(activity_id, owner_id):
     if not owner_id:
@@ -401,9 +262,7 @@ def handle_activity_create(activity_id, owner_id):
         return
 
     activity = activity_response.json()
-
     preferences = get_user_preferences(owner_id)
-
     total_calories_burnt = activity.get('calories', 0)
     beers_burnt = total_calories_burnt / 140
     pizza_slices_burnt = total_calories_burnt / 285
@@ -440,7 +299,6 @@ def handle_activity_create(activity_id, owner_id):
 def calculate_days_run_this_year(activities):
     today = datetime.datetime.today()
     start_of_year = datetime.datetime(today.year, 1, 1)
-
     run_dates = set()
 
     for activity in activities:
@@ -451,7 +309,6 @@ def calculate_days_run_this_year(activities):
 
     days_run = len(run_dates)
     total_days = (today - start_of_year).days + 1
-
     return days_run, total_days
 
 def calculate_kms_stats(activities):
@@ -470,7 +327,6 @@ def calculate_kms_stats(activities):
                 total_kms_run += activity['distance'] / 1000
 
     avg_kms_per_week = kms_last_4_weeks / 4
-
     return round(total_kms_run, 1), round(avg_kms_per_week, 1)
 
 def calculate_elevation_stats(activities):
@@ -489,8 +345,76 @@ def calculate_elevation_stats(activities):
                 total_elevation += activity['total_elevation_gain']
 
     avg_elevation_per_week = elevation_last_4_weeks / 4
-
     return round(total_elevation, 1), round(avg_elevation_per_week, 1)
+
+def get_user_preferences(owner_id):
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM user_preferences WHERE owner_id = %s", (owner_id,))
+        result = cursor.fetchone()
+        cursor.close()
+        if result:
+            return {
+                'days_run': result.get('days_run', True),
+                'total_kms': result.get('total_kms', True),
+                'avg_kms': result.get('avg_kms', True),
+                'total_elevation': result.get('total_elevation', False),
+                'avg_elevation': result.get('avg_elevation', False),
+                'avg_pace': result.get('avg_pace', False),
+                'avg_pace_per_week': result.get('avg_pace_per_week', False),
+                'beers_burnt': result.get('beers_burnt', False),
+                'pizza_slices_burnt': result.get('pizza_slices_burnt', False),
+                'remove_promo': result.get('remove_promo', False),
+                'is_paid_user': is_paid_user(owner_id)
+            }
+        else:
+            return {
+                'days_run': True,
+                'total_kms': True,
+                'avg_kms': True,
+                'total_elevation': False,
+                'avg_elevation': False,
+                'avg_pace': False,
+                'avg_pace_per_week': False,
+                'beers_burnt': False,
+                'pizza_slices_burnt': False,
+                'remove_promo': False,
+                'is_paid_user': is_paid_user(owner_id)
+            }
+    except mysql.connector.Error as err:
+        print(f"Error: {err.msg}")
+        return {
+            'days_run': True,
+            'total_kms': True,
+            'avg_kms': True,
+            'total_elevation': False,
+            'avg_elevation': False,
+            'avg_pace': False,
+            'avg_pace_per_week': False,
+            'beers_burnt': False,
+            'pizza_slices_burnt': False,
+            'remove_promo': False,
+            'is_paid_user': is_paid_user(owner_id)
+        }
+    finally:
+        if connection:
+            connection.close()
+
+def is_paid_user(athlete_id):
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute("SELECT is_paid_user FROM strava_tokens WHERE athlete_id = %s", (athlete_id,))
+        result = cursor.fetchone()
+        cursor.close()
+        return result['is_paid_user'] == 1
+    except mysql.connector.Error as err:
+        print(f"Error: {err.msg}")
+        return False
+    finally:
+        if connection:
+            connection.close()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
